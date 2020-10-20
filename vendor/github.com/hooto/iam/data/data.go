bdb2284578897a781a002a1b7abfe9f6418b3e27
Eryx <evorui@gmail.com>
2020-10-20 20:17:04 +0800

    [NEW] govendor init

---
+++ vendor/github.com/hooto/iam/data/data.go
@@ -0,0 +1,454 @@
+// Copyright 2014 Eryx <evorui аt gmаil dοt cοm>, All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package data
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/hooto/hauth/go/hauth/v1"
+	"github.com/hooto/hlog4g/hlog"
+	"github.com/lessos/lessgo/crypto/idhash"
+	"github.com/lessos/lessgo/crypto/phash"
+	"github.com/lessos/lessgo/net/email"
+	"github.com/lessos/lessgo/types"
+	kv2 "github.com/lynkdb/kvspec/go/kvspec/v2"
+
+	"github.com/hooto/iam/config"
+	"github.com/hooto/iam/iamapi"
+)
+
+var (
+	Data                    kv2.ClientTable
+	def_sysadmin            = "sysadmin"
+	DefaultSysadminPassword = "changeme"
+	app_inst_id_re          = regexp.MustCompile("^[0-9a-f]{16}$")
+	KeyMgr                  = hauth.NewAccessKeyManager()
+)
+
+func Setup() error {
+
+	if Data == nil {
+		return fmt.Errorf("iam.data connect not ready #1")
+	}
+
+	if rs := Data.NewWriter([]byte("iam:test"), "test").
+		ExpireSet(1000).Commit(); !rs.OK() {
+		return fmt.Errorf("iam.data connect not ready #2 " + rs.String())
+
+	} else if rs = Data.NewReader([]byte("iam:test")).Query(); !rs.OK() || rs.DataValue().String() != "test" {
+		return fmt.Errorf("iam.data connect not ready #3")
+	} else {
+		hlog.Printf("info", "iam/data connect ok")
+	}
+
+	// AccessKey
+	var (
+		ak0 = iamapi.NsAccessKey("", "")
+		akz = append(iamapi.NsAccessKey("", ""), []byte{0xff}...)
+		akn = 0
+	)
+	for {
+
+		rs := Data.NewReader().KeyRangeSet(ak0, akz).
+			LimitNumSet(1000).Query()
+		if !rs.OK() {
+			break
+		}
+
+		for _, v := range rs.Items {
+
+			ak0 = v.Meta.Key
+
+			var ak hauth.AccessKey
+			if err := v.Decode(&ak); err == nil {
+				KeyMgr.KeySet(&ak)
+				hlog.Printf("info", "iam/access_key load %s", ak.Id)
+				continue
+			}
+		}
+
+		akn += len(rs.Items)
+
+		if len(rs.Items) < 1000 {
+			break
+		}
+	}
+	hlog.Printf("info", "iam/access_key data load %d", akn)
+
+	for _, v := range config.Config.AccessKeys {
+		KeyMgr.KeySet(v)
+	}
+	hlog.Printf("info", "iam/access_key conf load %d", len(config.Config.AccessKeys))
+
+	return nil
+}
+
+func InitData() (err error) {
+
+	if err := Setup(); err != nil {
+		return err
+	}
+
+	if len(config.Config.InstanceID) < 16 {
+		return fmt.Errorf("No InstanceID Setup")
+	}
+
+	tnm := types.MetaTimeNow()
+
+	for _, v := range []iamapi.UserRole{
+		{
+			Id:   1,
+			Name: "sysadmin",
+			Desc: "Root System Administrator",
+		},
+		{
+			Id:   100,
+			Name: "member",
+			Desc: "Universal Member",
+		},
+		{
+			Id:   101,
+			Name: "developer",
+			Desc: "Universal Developer",
+		},
+		{
+			Id:   1000,
+			Name: "guest",
+			Desc: "Anonymous Guest",
+		},
+	} {
+
+		v.User = def_sysadmin
+		v.Status = 1
+		v.Created = tnm
+		v.Updated = tnm
+
+		rw := Data.NewWriter(iamapi.ObjKeyRole(v.Name), v).
+			IncrNamespaceSet("role").ModeCreateSet(true)
+		rw.Meta.IncrId = uint64(v.Id)
+		if rs := rw.Commit(); !rs.OK() {
+			return fmt.Errorf("db err %s", rs.Message)
+		}
+	}
+
+	//
+	ps := []iamapi.AppPrivilege{
+		{
+			Privilege: "sys.admin",
+			Roles:     []uint32{1},
+			Desc:      "System Management",
+		},
+		{
+			Privilege: "user.admin",
+			Roles:     []uint32{1},
+			Desc:      "User Management",
+		},
+	}
+
+	inst := iamapi.AppInstance{
+		Meta: types.InnerObjectMeta{
+			ID:      config.Config.InstanceID,
+			User:    def_sysadmin,
+			Created: types.MetaTimeNow(),
+			Updated: types.MetaTimeNow(),
+		},
+		Version:    config.Version,
+		AppID:      "iam",
+		AppTitle:   "hooto IAM Service",
+		Status:     1,
+		Url:        "",
+		Privileges: ps,
+	}
+
+	if rs := Data.NewWriter(iamapi.ObjKeyAppInstance(inst.Meta.ID), inst).
+		ModeCreateSet(true).Commit(); !rs.OK() {
+		return fmt.Errorf("db err %s", rs.Message)
+	}
+
+	AppInstanceRegister(inst)
+
+	/*
+		// privilege
+		rps := map[uint32][]string{}
+		for _, v := range ps {
+
+			for _, rid := range v.Roles {
+
+				if _, ok := rps[rid]; !ok {
+					rps[rid] = []string{}
+				}
+
+				rps[rid] = append(rps[rid], v.Privilege)
+			}
+		}
+
+		for rid, v := range rps {
+			PoPut(fmt.Sprintf("role-privilege/%d/%s", rid, inst.Meta.ID), strings.Join(v, ","), nil)
+		}
+	*/
+
+	// Init Super SysAdmin Account
+	// uid := idhash.HashToHexString([]byte(def_sysadmin), 8)
+	if rs := Data.NewReader(iamapi.ObjKeyUser(def_sysadmin)).Query(); rs.NotFound() {
+
+		sysadm := iamapi.User{
+			// Id:          uid,
+			Name:        def_sysadmin,
+			DisplayName: "System Administrator",
+			Email:       "",
+			Roles:       []uint32{1, 100},
+			Status:      1,
+			Created:     tnm,
+			Updated:     tnm,
+		}
+
+		auth, err := phash.Generate(DefaultSysadminPassword)
+		if err != nil {
+			return err
+		}
+
+		sysadm.Keys.Set(iamapi.UserKeyDefault, auth)
+
+		ow := Data.NewWriter(iamapi.ObjKeyUser(def_sysadmin), sysadm).
+			ModeCreateSet(true).IncrNamespaceSet("user")
+		ow.Meta.IncrId = 1
+		if rs := ow.Commit(); !rs.OK() {
+			return fmt.Errorf("db err %s", rs.Message)
+		}
+
+		var (
+			tn     = time.Now()
+			set_id = append(iamapi.Uint32ToBytes(uint32(tn.Unix())), idhash.Rand(8)...) // 4 + 8
+		)
+
+		set := iamapi.AccountFund{
+			Id:               iamapi.BytesToHexString(set_id),
+			Amount:           100000,
+			Type:             iamapi.AccountCurrencyTypeVirtual,
+			User:             def_sysadmin,
+			Operator:         def_sysadmin,
+			ExpProductMax:    100,
+			ExpProductLimits: []types.NameIdentifier{"sys/pod"},
+			Created:          types.MetaTimeSet(tn),
+			Updated:          types.MetaTimeSet(tn),
+			Priority:         8,
+		}
+
+		acc_user := iamapi.AccountUser{
+			User:    def_sysadmin,
+			Balance: set.Amount,
+			Updated: set.Updated,
+		}
+
+		sets := []kv2.ClientObjectItem{
+			{
+				Key:   iamapi.ObjKeyAccFundMgr(set.Id),
+				Value: set,
+			},
+			{
+				Key:   iamapi.ObjKeyAccFundUser(set.User, set.Id),
+				Value: set,
+			},
+			{
+				Key:   iamapi.ObjKeyAccUser(set.User),
+				Value: acc_user,
+			},
+		}
+
+		for _, v := range sets {
+			Data.NewWriter(v.Key, v.Value).Commit()
+		}
+	}
+
+	return nil
+}
+
+func SysConfigRefresh() {
+
+	//
+	if rs := Data.NewReader(iamapi.ObjKeySysConfig("mailer")).Query(); rs.OK() {
+
+		var mailer iamapi.SysConfigMailer
+
+		if err := rs.DataValue().Decode(&mailer, nil); err == nil && mailer.SmtpHost != "" {
+
+			email.MailerRegister(
+				"def",
+				mailer.SmtpHost,
+				mailer.SmtpPort,
+				mailer.SmtpUser,
+				mailer.SmtpPass,
+			)
+		}
+	}
+
+	if rs := Data.NewReader(iamapi.ObjKeySysConfig("service_name")).Query(); rs.OK() &&
+		rs.DataValue().String() != "" {
+		config.Config.ServiceName = rs.DataValue().String()
+	}
+
+	if rs := Data.NewReader(iamapi.ObjKeySysConfig("webui_banner_title")).Query(); rs.OK() &&
+		rs.DataValue().String() != "" {
+		config.Config.WebUiBannerTitle = rs.DataValue().String()
+	}
+
+	if rs := Data.NewReader(iamapi.ObjKeySysConfig("service_login_form_alert_msg")).Query(); rs.OK() {
+		config.Config.ServiceLoginFormAlertMsg = rs.DataValue().String()
+	}
+
+	if rs := Data.NewReader(iamapi.ObjKeySysConfig("user_reg_disable")).Query(); rs.OK() {
+
+		if rs.DataValue().String() == "1" {
+			config.UserRegistrationDisabled = true
+		} else {
+			config.UserRegistrationDisabled = false
+		}
+	}
+}
+
+func AccessKeyInitData(ak *hauth.AccessKey) error {
+
+	if ak.User == "" {
+		ak.User = "sysadmin"
+	}
+
+	ak.Status = hauth.AccessKeyStatusActive
+
+	if rs := Data.NewReader(iamapi.NsAccessKey(ak.User, ak.Id)).Query(); rs.OK() {
+		var prev hauth.AccessKey
+		if err := rs.Decode(&prev); err != nil {
+			return err
+		}
+		if !ak.Equal(&prev) {
+			if rs := Data.NewWriter(iamapi.NsAccessKey(ak.User, ak.Id), ak).
+				Commit(); !rs.OK() {
+				return rs.Error()
+			}
+			hlog.Printf("warn", "IAM/AK ID %s, SEC %s, Refreshed", ak.Id, ak.Secret[:8])
+		}
+		hlog.Printf("warn", "IAM/AK ID %s, SEC %s, Refreshed", ak.Id, ak.Secret[:8])
+	} else {
+
+		if rs := Data.NewWriter(iamapi.NsAccessKey(ak.User, ak.Id), ak).
+			ModeCreateSet(true).Commit(); !rs.OK() {
+			return rs.Error()
+		}
+		hlog.Printf("warn", "IAM/AK ID %s, SEC %s, Refreshed", ak.Id, ak.Secret[:8])
+	}
+
+	KeyMgr.KeySet(ak)
+
+	return nil
+}
+
+func AccessKeyReset(ak *hauth.AccessKey) error {
+
+	if ak.User == "" {
+		return errors.New("No User Set")
+	}
+
+	ak.Status = hauth.AccessKeyStatusActive
+
+	if rs := Data.NewWriter(iamapi.NsAccessKey(ak.User, ak.Id), ak).
+		Commit(); !rs.OK() {
+		return rs.Error()
+	}
+
+	KeyMgr.KeySet(ak)
+
+	return nil
+}
+
+func AppInstanceRegister(inst iamapi.AppInstance) error {
+
+	if !app_inst_id_re.MatchString(inst.Meta.ID) {
+		return fmt.Errorf("Invalid meta.id (%s)", inst.Meta.ID)
+	}
+
+	if rs := Data.NewReader(iamapi.ObjKeyUser(inst.Meta.User)).Query(); rs.NotFound() {
+		inst.Meta.User = def_sysadmin
+	}
+
+	var prev iamapi.AppInstance
+	if rs := Data.NewReader(iamapi.ObjKeyAppInstance(inst.Meta.ID)).Query(); rs.OK() {
+		rs.DataValue().Decode(&prev, nil)
+	}
+
+	if prev.Meta.ID == "" {
+		inst.Meta.Created = types.MetaTimeNow()
+		inst.Meta.Updated = types.MetaTimeNow()
+
+		Data.NewWriter(iamapi.ObjKeyAppInstance(inst.Meta.ID), inst).
+			ModeCreateSet(true).Commit()
+
+	} else {
+
+		prev.Meta.Updated = types.MetaTimeNow()
+
+		if inst.Version != "" && inst.Version != prev.Version {
+			prev.Version = inst.Version
+		}
+
+		if inst.AppTitle != "" && inst.AppTitle != prev.AppTitle {
+			prev.AppTitle = inst.AppTitle
+		}
+
+		if inst.Status != prev.Status {
+			prev.Status = inst.Status
+		}
+
+		if inst.Url != "" && inst.Url != prev.Url {
+			prev.Url = inst.Url
+		}
+
+		if inst.SecretKey != "" && inst.SecretKey != prev.SecretKey {
+			prev.SecretKey = inst.SecretKey
+		}
+
+		Data.NewWriter(iamapi.ObjKeyAppInstance(prev.Meta.ID), prev).
+			ModeCreateSet(true).Commit()
+
+		// TODO remove unused privileges
+	}
+
+	// privilege
+	rps := map[uint32][]string{}
+	for _, v := range inst.Privileges {
+
+		for _, rid := range v.Roles {
+
+			if _, ok := rps[rid]; !ok {
+				rps[rid] = []string{}
+			}
+
+			rps[rid] = append(rps[rid], v.Privilege)
+		}
+	}
+
+	for rid, v := range rps {
+
+		if rs := Data.NewWriter(
+			iamapi.ObjKeyRolePrivilege(rid, inst.Meta.ID), strings.Join(v, ",")).
+			ModeCreateSet(true).Commit(); !rs.OK() {
+			return fmt.Errorf("db err %s", rs.Message)
+		}
+	}
+
+	return nil
+}
---
+++ vendor/github.com/hooto/iam/iamclient/client.go
@@ -0,0 +1,252 @@
+// Copyright 2014 Eryx <evorui аt gmаil dοt cοm>, All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package iamclient
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"sync"
+	"time"
+
+	"github.com/lessos/lessgo/encoding/json"
+	"github.com/lessos/lessgo/net/httpclient"
+	"github.com/lessos/lessgo/types"
+
+	"github.com/hooto/hauth/go/hauth/v1"
+	"github.com/hooto/httpsrv"
+	"github.com/hooto/iam/iamapi"
+)
+
+const (
+	AccessTokenKey = iamapi.AccessTokenKey
+)
+
+var (
+	InstanceID         = ""
+	InstanceOwner      = ""
+	ServiceUrl         = "http://127.0.0.1:9528/iam"
+	ServiceUrlFrontend = ""
+	ServiceUrlGlobal   = ""
+)
+
+var (
+	locker      sync.RWMutex
+	c_roles           = iamapi.UserRoleList{}
+	c_roles_tto int64 = 0
+)
+
+func Expired(ttl int) time.Time {
+	return time.Now().Add(time.Second * time.Duration(ttl))
+}
+
+func service_url_global() string {
+	if ServiceUrlGlobal != "" {
+		return ServiceUrlGlobal
+	}
+	return ServiceUrl
+}
+
+func service_prefix() string {
+
+	if ServiceUrlFrontend != "" {
+		return ServiceUrlFrontend
+	}
+
+	return ServiceUrl
+}
+
+func LoginUrl(backurl string) string {
+	return service_prefix() + "/service/login?continue=" + backurl
+}
+
+func AuthServiceUrl(client_id, redirect_uri, state string) string {
+	return auth_service_url(service_prefix(), client_id, redirect_uri, state)
+}
+
+func auth_service_url(service_url, client_id, redirect_uri, state string) string {
+	return fmt.Sprintf(
+		"%s/service/login?response_type=token&client_id=%s&redirect_uri=%s&state=%s",
+		service_url, client_id, url.QueryEscape(redirect_uri), url.QueryEscape(state),
+	)
+}
+
+func SessionAccessToken(s *httpsrv.Session) string {
+	return s.Get(AccessTokenKey)
+}
+
+func SessionIsLogin(s *httpsrv.Session) bool {
+
+	if s == nil {
+		return false
+	}
+
+	return tokenIsLogin(s.Get(AccessTokenKey))
+}
+
+func SessionAccessAllowed(s *httpsrv.Session, privilege, client_id string) bool {
+
+	if s == nil {
+		return false
+	}
+
+	return tokenAccessAllowed(privilege, s.Get(AccessTokenKey), client_id)
+}
+
+func SessionInstance(s *httpsrv.Session) (iamapi.UserSession, error) {
+
+	if s == nil {
+		return iamapi.UserSession{}, errors.New("No Session Found")
+	}
+
+	sess, err := Instance(s.Get(AccessTokenKey))
+	if err != nil {
+		return iamapi.UserSession{}, err
+	}
+
+	return *sess, nil
+}
+
+func Instance(token string) (*iamapi.UserSession, error) {
+
+	if ServiceUrl == "" || token == "" {
+		return nil, errors.New("Unauthorized")
+	}
+
+	ap, err := hauth.NewUserValidator(token, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if ap.IsExpired() {
+		return nil, errors.New("auth expired")
+	}
+
+	tn := time.Now().Unix()
+
+	// cache
+	session := SessionCache(ap.Id)
+	if session != nil && !sessionCacheRefresh(session, tn) {
+		return session, nil
+	}
+
+	hc := httpclient.Get(fmt.Sprintf(
+		"%s/v1/service/auth?%s=%s",
+		ServiceUrl,
+		AccessTokenKey,
+		token,
+	))
+	hc.SetTimeout(3000)
+	defer hc.Close()
+
+	var us types.TypeMeta
+	if err = hc.ReplyJson(&us); err != nil {
+		err = errors.New("Network error, please try again later")
+	} else if us.Kind != "UserSession" {
+		if us.Error != nil {
+			err = errors.New("Unauthorized " + us.Error.Message)
+		} else {
+			err = errors.New("Unauthorized")
+		}
+	} else {
+
+		session = &iamapi.UserSession{
+			AccessToken: token,
+			UserName:    ap.Id,
+			DisplayName: ap.Name,
+			Roles:       ap.Roles,
+			Groups:      ap.Groups,
+			Expired:     ap.Expired,
+		}
+		SessionSync(session, tn)
+	}
+
+	if err != nil && session == nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func tokenIsLogin(token string) bool {
+
+	if _, err := Instance(token); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func tokenAccessAllowed(privilege, token, instanceid string) bool {
+
+	if !tokenIsLogin(token) {
+		return false
+	}
+
+	req := iamapi.UserAccessEntry{
+		AccessToken: token,
+		InstanceID:  instanceid,
+		Privilege:   privilege,
+	}
+
+	js, _ := json.Encode(req, "")
+	hc := httpclient.Post(ServiceUrl + "/v1/service/access-allowed")
+	hc.Header("contentType", "application/json; charset=utf-8")
+	hc.SetTimeout(3000)
+	hc.Body(js)
+	defer hc.Close()
+
+	var us iamapi.UserAccessEntry
+	if err := hc.ReplyJson(&us); err != nil || us.Kind != "UserAccessEntry" {
+		return false
+	}
+
+	return true
+}
+
+func AppRoleList(s *httpsrv.Session, appid string) (*iamapi.UserRoleList, error) {
+
+	token := s.Get(AccessTokenKey)
+	if ServiceUrl == "" || token == "" {
+		return nil, errors.New("Unauthorized")
+	}
+
+	tnu := time.Now().UTC().Unix()
+	if c_roles_tto > tnu {
+		return &c_roles, nil
+	}
+
+	hc := httpclient.Get(fmt.Sprintf(
+		"%s/v1/app-auth/role-list?%s=%s&appid=%s",
+		ServiceUrl,
+		AccessTokenKey,
+		token,
+		appid,
+	))
+	hc.SetTimeout(3000)
+	defer hc.Close()
+
+	var rls iamapi.UserRoleList
+	if err := hc.ReplyJson(&rls); err != nil {
+		return nil, err
+	} else if rls.Error != nil || rls.Kind != "UserRoleList" {
+		return nil, errors.New("Network is unreachable, Please try again later")
+	}
+
+	c_roles_tto = tnu
+	c_roles = rls
+
+	return &c_roles, nil
+}
